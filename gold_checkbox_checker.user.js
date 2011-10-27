cd5820be52fae3e78066b1597a75894243ed634b
Nelson Chen <crazysim@gmail.com>
2011-10-26 20:14:35 -0700

    logging out redirects to gold with a captical Login.aspx

--- gold_checkbox_checker.user.js
+++ gold_checkbox_checker.user.js
@@ -1,6 +1,7 @@
 // ==UserScript==
 // @name UCSB GOLD Login Checkbox Checker
 // @match https://my.sa.ucsb.edu/gold/login.aspx
+// @match https://my.sa.ucsb.edu/gold/Login.aspx
 // @description GOLD, thieves lie. I am not better protected. Just more annoyed.
 // ==/UserScript==
 
